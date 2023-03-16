b3015265369d6a4c659720e7a61be5c0fe4d9a3e
Leaozinho <Leaozinho@LEAOZINHO>
2023-03-16 10:30:04 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Calculadora2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxOperacao = new System.Windows.Forms.ComboBox();
+            this.lblIgual = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxOperacao
+            // 
+            this.comboBoxOperacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxOperacao.FormattingEnabled = true;
+            this.comboBoxOperacao.Location = new System.Drawing.Point(157, 27);
+            this.comboBoxOperacao.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBoxOperacao.Name = "comboBoxOperacao";
+            this.comboBoxOperacao.Size = new System.Drawing.Size(85, 37);
+            this.comboBoxOperacao.TabIndex = 1;
+            // 
+            // lblIgual
+            // 
+            this.lblIgual.BackColor = System.Drawing.SystemColors.Control;
+            this.lblIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIgual.Location = new System.Drawing.Point(362, 27);
+            this.lblIgual.Name = "lblIgual";
+            this.lblIgual.Size = new System.Drawing.Size(61, 37);
+            this.lblIgual.TabIndex = 3;
+            this.lblIgual.Text = "=";
+            this.lblIgual.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(416, 30);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(113, 37);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor1.Location = new System.Drawing.Point(27, 30);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 34);
+            this.txtValor1.TabIndex = 5;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor2.Location = new System.Drawing.Point(270, 30);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 34);
+            this.txtValor2.TabIndex = 6;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.Location = new System.Drawing.Point(351, 120);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(122, 39);
+            this.btnResultado.TabIndex = 7;
+            this.btnResultado.Text = "Calcular";
+            this.btnResultado.UseVisualStyleBackColor = false;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Valor 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(308, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Valor 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 171);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblIgual);
+            this.Controls.Add(this.comboBoxOperacao);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBoxOperacao;
+        private System.Windows.Forms.Label lblIgual;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora2._0
+{
+    public partial class Form1 : Form
+
+    {
+        double resultado;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Vetor com as operações que entrarão no comboBox
+            string[] operacao = new string[4] { "+", "-", "x", "/" };
+           
+            // Inserindo as operações dentro do comboBox
+            foreach (String op in operacao)
+            {
+                comboBoxOperacao.Items.Add(op.ToString());
+
+            }
+
+
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            // Verifica se os números inseridos são numéricos
+            // tenta converter o txtValor1 em double e armazena na variável valor1 || tenta converter o txtValor2 em double e armazena na variável valor2
+            // o código verifica se as strings nos campos de texto txtValor1 e txtValor2 podem ser convertidas em números double, se a conversão falhar é exibida uma menssagem de erro
+            if (!double.TryParse(txtValor1.Text, out double valor1) || !double.TryParse(txtValor2.Text, out double valor2))
+            {
+                MessageBox.Show("Os valores inseridos devem ser numéricos.");
+                return;
+            }
+
+            // Verifica se o divisor é diferente de zero
+            // se o divisor for 0, a menssagem de erro é disparada
+            if (comboBoxOperacao.SelectedItem.ToString() == "/" && valor2 == 0)
+            {
+                MessageBox.Show("Não é possível dividir por zero.");
+                return;
+            }
+
+            
+
+            
+    
+
+            // o selecteIndex permite que eu acesse os itens do comboBox pelo indice
+            switch (comboBoxOperacao.SelectedIndex)
+            {
+                // se o indice for 0 ou seja o "+"
+                case 0:
+                    resultado = valor1 + valor2;
+
+                    break;
+                // se o indice for 1 ou seja o "-"
+                case 1:
+                    resultado = valor1 - valor2;
+       
+                    break;
+                case 2:
+                    // se o indice for 2 ou seja o "x"
+                    resultado = valor1 * valor2;
+                    
+                    break;
+                case 3:
+                    // se o indice for 3 ou seja a "/"
+                    resultado = valor1 / valor2;
+                   
+                    break;
+
+            }
+            // sendo uma das condições satisfeitas, eu exibo no meu lblResultado o resultado da operação
+            // o resultado entra convertido em string pois o lblResultado é um campo do tipo que aceita somente texto
+
+            lblResultado.Text = resultado.ToString();
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
